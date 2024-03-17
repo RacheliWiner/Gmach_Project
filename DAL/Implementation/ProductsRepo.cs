d6a8463077c2bb4d790f69f808191026a55238c0
וינר רחל1 < 1@C10>
2024-03-17 17:04:00 +0200

    controller location #3

--- DAL/Implementation/ProductsRepo.cs
+++ DAL/Implementation/ProductsRepo.cs
@@ -1,54 +1,54 @@
-﻿//using DAL.Api;
-//using DAL.DalObject;
-//using ProductRepo = DAL.DalObject.ProductRepo;
+﻿using DAL.Api;
+using DAL.DalObject;
+using ProductRepo = DAL.DalObject.ProductRepo;
 
-//namespace DAL.Implementation
-//{
-//    internal class ProductsRepo : IRepository<ProductRepo>
-//    {
-//        GmachContext context;
-//        public ProductsRepo(GmachContext context)
-//        {
-//            this.context = context;
-//        }
-//        public List<ProductRepo> GetAll()
-//        {
-//            return context.Products.ToList();
-//        }
-//        public void Update(int id, ProductRepo item)
-//        {
-//            var entity = GetById(id);
-//            context.Products.Add(entity);//מה צריך לעדכן פה?
-//            entity.ProductName = item.ProductName;
-//            entity.ProductDescription = item.ProductDescription;
-//            entity.ProductCode = item.ProductCode;
-//            entity.Price = item.Price;
-//            entity.Amount = item.Amount;
-//            entity.LeftInStock = item.LeftInStock;
-//            context.SaveChanges();
-
-
-//        }
-//        public ProductRepo GetById(int id)
-//        {
-//            return context.Products.Find(id);
-//        }
+namespace DAL.Implementation
+{
+    internal class ProductsRepo : IRepository<ProductRepo>
+    {
+        GmachContext context;
+        public ProductsRepo(GmachContext context)
+        {
+            this.context = context;
+        }
+        public List<ProductRepo> GetAll()
+        {
+            return context.Products.ToList();
+        }
+        public void Update(int id, ProductRepo item)
+        {
+            var entity = GetById(id);
+            context.Products.Add(entity);//מה צריך לעדכן פה?
+            entity.ProductName = item.ProductName;
+            entity.ProductDescription = item.ProductDescription;
+            entity.ProductCode = item.ProductCode;
+            entity.Price = item.Price;
+            entity.Amount = item.Amount;
+            entity.LeftInStock = item.LeftInStock;
+            context.SaveChanges();
 
 
-//        void IRepository<ProductRepo>.Delete(int id)
-//        {
-//            var entity = GetById(id);
-//            context.Products.Remove(entity);
-//            context.SaveChanges();
+        }
+        public ProductRepo GetById(int id)
+        {
+            return context.Products.Find(id);
+        }
 
-//        }
 
-//        void IRepository<ProductRepo>.Add(ProductRepo item)
-//        {
-//            context.Products.Add(item);
-//            context.SaveChanges();
-//            return;
+        void IRepository<ProductRepo>.Delete(int id)
+        {
+            var entity = GetById(id);
+            context.Products.Remove(entity);
+            context.SaveChanges();
 
-//        }
-//    }
-//}
+        }
+
+        void IRepository<ProductRepo>.Add(ProductRepo item)
+        {
+            context.Products.Add(item);
+            context.SaveChanges();
+            return;
+
+        }
+    }
+}
