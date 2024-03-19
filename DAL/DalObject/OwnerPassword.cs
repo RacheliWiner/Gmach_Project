b15974caa00a5cd1616bbba7fc5cbfc4f370f797
גוטפרוינד מיכל < @C11>
2024-03-19 15:56:40 +0200

    scaffold #2

---
+++ DAL/DalObject/OwnerPassword.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL.DALObject;
+
+public partial class OwnerPassword
+{
+    public int GmachCode { get; set; }
+
+    public string OwnerPassword1 { get; set; }
+
+    public virtual Gmach GmachCodeNavigation { get; set; }
+}
