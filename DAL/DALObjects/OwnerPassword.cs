695a693557e40709516c6811644848e4bf971e2d
וינר רחל1 < 1@C9>
2024-03-19 16:57:38 +0200

    scaffold #3

---
+++ DAL/DALObjects/OwnerPassword.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL.DALObjects;
+
+public partial class OwnerPassword
+{
+    public int GmachCode { get; set; }
+
+    public string OwnerPassword1 { get; set; }
+
+    public virtual Gmach GmachCodeNavigation { get; set; }
+}
