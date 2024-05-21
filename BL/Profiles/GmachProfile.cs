2ad08068b6c1179ffa8758ca1af7480b7c567ba0
וינר רחל1 < 1@C9>
2024-05-21 17:37:02 +0300

    solved bug

---
+++ BL/Profiles/GmachProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BL.BLObject;
+using DAL.DALObjects;
+
+namespace BL.Profiles
+{
+    public class GmachProfile: Profile
+    {
+        public GmachProfile()
+        {
+            CreateMap<Gmach,GmachDetailsForClient>().ReverseMap();
+        }
+    }
+}
